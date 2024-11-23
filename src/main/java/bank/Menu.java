fb3e61ee987c29254ee42d3f32a6bcf7d3b85187
Kinga <37161970+madking55@users.noreply.github.com>
2024-11-23 10:07:05 +0000

    add Menu options

--- src/main/java/bank/Menu.java
+++ src/main/java/bank/Menu.java
@@ -8,8 +8,7 @@ public class Menu {
 
   private Scanner scanner;
 
-
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("Welcome to ABCD Bank");
 
     Menu menu = new Menu();
@@ -20,20 +19,19 @@ public class Menu {
     if (customer != null) {
       Account account = DataSource.getAccount(customer.getAccountId());
       menu.showMenu(customer, account);
-          }
-      
-          menu.scanner.close();
-        }
-      
-      
-        private Customer authenticateUser(){
+    }
+
+    menu.scanner.close();
+  }
+
+  private Customer authenticateUser() {
     System.out.println("Please enter your username");
     String username = scanner.next();
 
     System.out.println("Please provide your password");
     String password = scanner.next();
 
-    Customer customer= null;
+    Customer customer = null;
     try {
       customer = Authenticator.login(username, password);
     } catch (LoginException e) {
@@ -43,8 +41,48 @@ public class Menu {
     return customer;
   }
 
-  private void showMenu(Customer customer, Account account){
-    
+  private void showMenu(Customer customer, Account account) {
+
+    int selection = 0;
+
+    while (selection != 4 && customer.isAuthenticated()) {
+      System.out.println("=============================================");
+      System.out.println("Please chose one of the following options:");
+      System.out.println("1: Deposit");
+      System.out.println("2: Withdraw");
+      System.out.println("3: Check balance");
+      System.out.println("4: Exit");
+      System.out.println("=============================================");
+
+      selection = scanner.nextInt();
+      double amount = 0;
+
+      switch (selection) {
+        case 1:
+          System.out.println("How much you want to deposit?");
+          amount = scanner.nextDouble();
+          account.deposit(amount);
+          break;
+
+        case 2:
+          System.out.println("How much you want to withdraw?");
+          amount = scanner.nextDouble();
+          account.withdraw(amount);
+          break;
+
+        case 3:
+          System.out.println("Current balance: " + account.getBalance());
+          break;
+
+        case 4:
+          Authenticator.logout(customer);
+          System.out.println("Thanks for banking with ABCD Bank");
+          break;
+
+        default:
+          System.out.println("Inavlid option,please try again ");
+      }
+    }
+
   }
 }
- 
